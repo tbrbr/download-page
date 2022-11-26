31c79191366cd2592eb000d3d0bac775a1c16eed
Dark Rooster <114775689+tbrbr@users.noreply.github.com>
2022-11-25 23:13:44 -0300

    Add files via upload

---
+++ animation/script.js
@@ -0,0 +1,16 @@
+let slides = document.querySelectorAll('.specificarea');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prox(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+setInterval(next, 10000);
